38f07a9af159864da4f2c14c08ae0bb5abe86b45
Jules Pinsard <jules.pinsard@gmail.com>
2023-11-20 17:51:12 +0100

    Evening commit

--- Gemfile
+++ Gemfile
@@ -3,6 +3,14 @@ source "https://rubygems.org"
 ruby "3.1.2"
 
 gem "devise"
+# Gemfile
+gem "dotenv-rails"
+
+# Gemfile
+gem "cloudinary"
+gem 'faker'
+
+
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_143449) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_164734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,26 +46,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_143449) do
     t.string "origin"
     t.string "destination"
     t.date "time_of_departure"
-    t.date "time_of_arrrival"
     t.integer "number_of_people"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.date "time_of_arrival"
     t.index ["user_id"], name: "index_trips_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
