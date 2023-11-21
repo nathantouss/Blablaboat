2698dec9251b4878cb2bf56c6ede18e159d65f59
Jules Pinsard <jules.pinsard@gmail.com>
2023-11-21 17:53:27 +0100

    Evening push

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-  # has_one_attached :photo
+  has_one_attached :photo
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,6 @@
     password: Devise::Encryptor.digest(User, Faker::Internet.password),
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name
-
   )
   puts user
 end
