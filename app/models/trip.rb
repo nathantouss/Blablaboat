56b2548b283a4fc6cd1d115b1648d86f28913981
Jules Pinsard <jules.pinsard@gmail.com>
2023-11-22 11:31:31 +0100

    Validation readyyyy

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -9,9 +9,9 @@ class Trip < ApplicationRecord
   validates :time_of_arrival, presence: true
   validates_datetime :time_of_arrival, { after: :time_of_departure,
                                          message: "must be after or same as time of departure" }
-  validates :number_of_people, presence: true
+  validates :number_of_people, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :user_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def seat_available
     seat_taken = 0
